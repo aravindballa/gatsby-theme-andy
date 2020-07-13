6e3a9c20470cadc5c4ef9ace008de5aff54ddbd9
Soumik Pradhan <soumikpradhan14@gmail.com>
2020-07-13 02:40:37 +0000

    chore(indent): use 2 spaces instead of tabs (4 width)

--- src/components/CustomLinkToStacked.js
+++ src/components/CustomLinkToStacked.js
@@ -3,81 +3,81 @@ import { GatsbyLinkProps, Link  } from "gatsby";
 import { useStackedPage } from "react-stacked-pages-hook";
 
 export const LinkToStacked = React.forwardRef(
-	(
-		{
-			to,
-			onClick,
-			onMouseLeave,
-			onMouseEnter,
-			...restProps
+  (
+    {
+      to,
+      onClick,
+      onMouseLeave,
+      onMouseEnter,
+      ...restProps
 
-		},
-		ref
-	) => {
-		const [
-			,
-			,
-			,
-			navigateToStackedPage,
-			highlightStackedPage,
-		] = useStackedPage();
-		const onClickHandler = useCallback(
-			(ev) => {
-				ev.preventDefault();
-				if (onClick)
-					onClick(ev);
+    },
+    ref
+  ) => {
+    const [
+      ,
+      ,
+      ,
+      navigateToStackedPage,
+      highlightStackedPage,
+    ] = useStackedPage();
+    const onClickHandler = useCallback(
+      (ev) => {
+        ev.preventDefault();
+        if (onClick)
+          onClick(ev);
 
-				const isMac = window.navigator.platform.toUpperCase().indexOf('MAC') >= 0;
+        const isMac = window.navigator.platform.toUpperCase().indexOf('MAC') >= 0;
 
-				/* Override command + click on MacOS and Ctrl + click on other OS */
-				if (isMac && ev.metaKey || !isMac && ev.ctrlKey)
-					window.open(to, '_blank');
-				else
-					navigateToStackedPage(to);
+        /* Override command + click on MacOS and Ctrl + click on other OS */
+        if (isMac && ev.metaKey || !isMac && ev.ctrlKey)
+          window.open(to, '_blank');
+        else
+          navigateToStackedPage(to);
 
-			},
-			[navigateToStackedPage, to, onClick]
+      },
+      [navigateToStackedPage, to, onClick]
 
-		);
+    );
 
-		const onMouseEnterHandler = useCallback(
-			(ev) => {
-				highlightStackedPage(to, true);
-				if (onMouseEnter) {
-					onMouseEnter(ev);
+    const onMouseEnterHandler = useCallback(
+      (ev) => {
+        highlightStackedPage(to, true);
+        if (onMouseEnter) {
+          onMouseEnter(ev);
 
-				}
+        }
 
-			},
-			[to, onMouseEnter, highlightStackedPage]
+      },
+      [to, onMouseEnter, highlightStackedPage]
 
-		);
+    );
 
-		const onMouseLeaveHandler = useCallback(
-			(ev) => {
-				highlightStackedPage(to, false);
-				if (onMouseLeave) {
-					onMouseLeave(ev);
+    const onMouseLeaveHandler = useCallback(
+      (ev) => {
+        highlightStackedPage(to, false);
+        if (onMouseLeave) {
+          onMouseLeave(ev);
 
-				}
+        }
 
-			},
-			[to, onMouseLeave, highlightStackedPage]
+      },
+      [to, onMouseLeave, highlightStackedPage]
 
-		);
+    );
 
-		return (
-			<Link
-				{...restProps}
-				to={to}
-				ref={ref}
-				onClick={onClickHandler}
-				onMouseEnter={onMouseEnterHandler}
-				onMouseLeave={onMouseLeaveHandler}
-			/>
+    return (
+      <Link
+        {...restProps}
+        to={to}
+        ref={ref}
+        onClick={onClickHandler}
+        onMouseEnter={onMouseEnterHandler}
+        onMouseLeave={onMouseLeaveHandler}
+      />
 
-		);
+    );
 
-	}
+  }
 
 );
